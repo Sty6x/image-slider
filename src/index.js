8067fc68780fbd585c544699bbdaa621d9bc3eea
zaid <jerylfranz460@gmail.com>
2022-10-19 05:41:11 +0400

    changed placeholders to actual dots

--- src/index.js
+++ src/index.js
@@ -5,26 +5,35 @@ import telephoneImg from './resources/img/pexels-lokman-sevim-9704111.jpg'
 import cameraImg from './resources/img/pexels-tahir-osman-13859770.jpg'
 import machineImg from './resources/img/pexels-алесь-усцінаў-9932622.jpg'
 
-const myImg = new Image()
-// myImg.src = shopImg;
-// myImg.src = telephoneImg;
 const main = document.querySelector('main')
-const previousArrow = document.getElementById('previous-arrow')
-const nextArrow = document.getElementById('next-arrow')
+const img = document.getElementById('image')
 
+const imagesArr = [shopImg, tablesImg, telephoneImg, cameraImg, machineImg]
+img.src = imagesArr[0]
 
+let i = 0
 main.addEventListener('click', e => {
   const target = e.target;
+  slideImage(target)
+})
+
+function slideImage(target) {
+
   if (target.matches('#previous-arrow')) {
-    console.log(target)
-    changeImage(shopImg)
+    i--
+    i < 0 ? i = 0 : 'array out of scope'
+    console.log(i)
+    changeImage(imagesArr[i])
   }
   else if (target.matches('#next-arrow')) {
-    changeImage(telephoneImg)
-    console.log(target)
+    i++
+    i > 4 ? i = 4 : 'array out of scope'
+    console.log(i)
+    changeImage(imagesArr[i])
   }
 
-})
+}
+
 function changeImage(a) {
-  document.getElementById('image').src = a;
+  img.src = a;
 }
