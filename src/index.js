2d14d646dc6c77f04948df70d9b40d5bad92a1d2
zaid <jerylfranz460@gmail.com>
2022-10-19 06:02:31 +0400

    first or maybe last array returns error and doesnt change previous element's class

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import machineImg from './resources/img/pexels-алесь-усцінаў-9932622
 
 const main = document.querySelector('main')
 const img = document.getElementById('image')
+const indicator = document.getElementById('indicator')
+let i = 0
 
 const imagesArr = [shopImg, tablesImg, telephoneImg, cameraImg, machineImg]
 img.src = imagesArr[0]
 
-let i = 0
 main.addEventListener('click', e => {
   const target = e.target;
   slideImage(target)
@@ -23,16 +24,34 @@ function slideImage(target) {
     i--
     i < 0 ? i = 0 : 'array out of scope'
     console.log(i)
+    changeIndicator(i)
     changeImage(imagesArr[i])
   }
   else if (target.matches('#next-arrow')) {
     i++
     i > 4 ? i = 4 : 'array out of scope'
     console.log(i)
+    changeIndicator(i)
     changeImage(imagesArr[i])
   }
 
 }
+function changeIndicator(i) {
+  if (indicator.children[i].classList.contains('dot')) {
+    indicator.children[i].classList.remove('dot')
+    indicator.children[i].classList.add('active-dot')
+    console.log(indicator.children[i].previousElementSibling)
+    if (indicator.children[i].previousElementSibling.classList.contains('active-dot')) {
+      indicator.children[i].previousElementSibling.classList.remove('active-dot')
+      indicator.children[i].previousElementSibling.classList.add('dot')
+    }
+    else if (indicator.children[i].nextElementSibling.classList.contains('active-dot')) {
+      indicator.children[i].nextElementSibling.classList.add('dot')
+      indicator.children[i].nextElementSibling.classList.remove('active-dot')
+    }
+  }
+}
+
 
 function changeImage(a) {
   img.src = a;
